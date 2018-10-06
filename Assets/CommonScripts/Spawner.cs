7e0068f40cc9345b8bc83cc38d7b7d11c8c73221
handsonicv4 <sentaoli@usc.edu>
2018-10-06 16:25:07 -0700

    3

---
+++ Assets/CommonScripts/Spawner.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour {
+
+    public GameObject obj;
+    public uint amont = 7;
+
+    int number;
+	// Use this for initialization
+	void Start () {
+        number = 0;
+	}
+
+    float timer = 0;
+	// Update is called once per frame
+	void Update () {
+		
+        if(timer >= 1)
+        {
+
+        }
+
+
+
+	}
+}
